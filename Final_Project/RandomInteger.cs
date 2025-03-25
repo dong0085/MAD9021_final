c391782266c1891dfb00358e57dfb5c15c4cad43
Cheng Dong <dong0085@algonquinlive.com>
2025-03-25 17:45:06 -0400

    initial files

---
+++ Final_Project/RandomInteger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Project
+{
+    internal class RandomInteger
+    {
+        protected Random random = new Random(int.Parse(Guid.NewGuid().ToString().Substring(0, 8), System.Globalization.NumberStyles.HexNumber));
+        protected int currentRandomNumber = 0;
+
+        public RandomInteger()
+        {
+
+        }
+
+        public virtual int GenerateRandomNumber()
+        {
+            currentRandomNumber = random.Next();
+            return currentRandomNumber;
+        }
+
+        public int GetCurrentRandomNumber() { return currentRandomNumber; }
+    }
+}
---
+++ Final_Project/RangedRandomInteger.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Project
+{
+    internal class RangedRandomInteger : RandomInteger
+    {
+        protected int maximum;
+        protected int minimum;
+
+        public RangedRandomInteger()
+        {
+
+        }
+
+        public RangedRandomInteger(int minimum, int maximum)
+        {
+            SetMaximum(maximum);
+            SetMinimum(minimum);
+        }
+
+        // Setters
+
+        public void SetMaximum(int maximum)
+        {
+            if (maximum == 20 || maximum == 100 || maximum == 1000)
+            {
+                this.maximum = maximum;
+            }
+        }
+        public void SetMinimum(int minimum)
+        {
+            if (minimum == 1 || minimum == 10 || minimum == 100)
+            {
+                this.minimum = minimum;
+            }
+        }
+
+        // Getters
+        public int getMinimum()
+        {
+            return this.minimum;
+        }
+        public int getMaximum()
+        {
+            return this.maximum;
+        }
+
+        public override int GenerateRandomNumber()
+        {
+            int randomNum = random.Next(minimum, maximum);
+            return randomNum;
+        }
+
+    }
+}
